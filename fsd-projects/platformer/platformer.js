fcf3a4a05a21feac04ee967d302481f9b1571a82
dillan123456 <dillan.marshall@carvercollegiate.org>
2025-03-24 18:44:12 +0000

    bouncing box

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,11 +27,11 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
+    createPlatform(400,225,100,20);
 
 
 
